5d07844f25395f81ec2c5d4ae71b8da00b399888
David <davidmelos74@gmail.com>
2025-09-09 05:13:09 -0300

    atualizaçaõ do css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ function App() {
       <div className='back-ground'></div> 
       <div className='chat-container'>
         
+        <div className='chat-contacts'>
+          <div className='chat-options'></div>
+          <div className='chat-item'>
+            <img src={Image} className='image-profile' alt=''/>
+            <div className='title-chat-container'>
+              <span className='title-message'>Profissão programador</span>
+              <span className='last-message'>David: Bom dia!</span>
+            </div>
+          </div>
+        </div>
+      
       </div>   
     </div>
   );
