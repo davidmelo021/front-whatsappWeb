a27fb59dd96e6563d13828e1feb3f8bddbf3d0dc
David Melo <david.melos74@gmail.com>
2025-09-10 17:12:38 -0300

    Acabamento e iniciando back-end

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Image from './assets/profissao-programador.jpg';
+import SendMessageIcon from './assets/send.png';
 
 function App() {
   return (
@@ -16,7 +18,27 @@ function App() {
             </div>
           </div>
         </div>
-      
+          
+          <div className='chat-messages'>
+            <div className='chat-options'>
+              <div className='chat-item'>
+                <img src={Image} className='image-profile' alt=''/>
+                <div className='title-chat-container'>
+                 <span className='title-message'>Profissão programador</span>
+                 <span className='last-message'>David,Leticia,Lucineide</span>
+               </div>
+              </div>
+
+            </div>
+            <div className='chat-messages-area'>
+            
+            </div>
+            <div className='chat-input-area'>
+              <input className='chat-input' placeholder='Mensagem'/>
+              <img src={SendMessageIcon} alt='' className='send-message-icon'/>
+            </div>
+          </div>
+
       </div>   
     </div>
   );
